2cef5bdce1e7296b1dc07c0884b0a350e2855799
Craig Wechtler <cwechtler@yahoo.com>
2019-01-28 13:18:48 -0600

    UI Scaling bug Fix

--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -1,11 +1,17 @@
 ﻿
 using UnityEngine;
-
+using UnityStandardAssets._2D;
 public class Player : MonoBehaviour {
 
+	PlatformerCharacter2D platformerCharacter2D;
+
+	private void Start()
+	{
+		platformerCharacter2D = GetComponent<PlatformerCharacter2D>();
+	}
 	private void Update()
 	{
-		if (Input.GetButtonDown("Jump"))
+		if (Input.GetButtonDown("Jump") && platformerCharacter2D.Grounded)
 		{
 			SoundManager.instance.PlayJump();
 		}
